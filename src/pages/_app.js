196c6f6fcd4c12f058dc9fcb6816567d7c8d5bb1
Henry Betancourth <69738617+noelys215@users.noreply.github.com>
2023-07-11 23:00:00 +0900

    Add fonts

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,12 @@
-import '@/styles/globals.css'
+import '@/styles/globals.css';
+import { Spectral } from 'next/font/google';
+
+const spectral = Spectral({ subsets: ['latin'], weight: ['200', '300', '400', '500'] });
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+	return (
+		<main className={spectral.className}>
+			<Component {...pageProps} />
+		</main>
+	);
 }
