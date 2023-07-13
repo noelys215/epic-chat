b776ad8d9c0b14bd5cb69af2a2619c06d4073a4e
Henry Betancourth <69738617+noelys215@users.noreply.github.com>
2023-07-13 23:00:00 +0900

    Finish firebase.js

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -1,7 +1,8 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp, getApp } from 'firebase/app';
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getFirestore } from 'firebase/firestore';
+import { GoogleAuthProvider, getAuth } from 'firebase/auth';
+import { getStorage } from 'firebase/storage';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -15,3 +16,9 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = getApp().length === 0 ? initializeApp(firebaseConfig) : getApp()[0];
+const db = getFirestore(app);
+const auth = getAuth(app);
+const storage = getStorage(app);
+const provider = new GoogleAuthProvider();
+
+export { db, auth, storage, provider };
