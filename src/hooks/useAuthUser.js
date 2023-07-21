e1dea182ae8a0dcd285523ecb8d6e1b87520824a
Henry Betancourth <69738617+noelys215@users.noreply.github.com>
2023-07-20 21:18:23 -0400

    finish useAuthUser hook

--- src/hooks/useAuthUser.js
+++ src/hooks/useAuthUser.js
@@ -1,11 +1,24 @@
-import { auth } from '@/utils/firebase';
+import { auth, db } from '@/utils/firebase';
+import { doc, getDoc, serverTimestamp, setDoc } from 'firebase/firestore';
 import { useEffect } from 'react';
 import { useAuthState } from 'react-firebase-hooks/auth';
 
 export const useAuthUser = () => {
 	const [user] = useAuthState(auth);
+
 	useEffect(() => {
-		console.log(user);
+		if (user) {
+			const userRef = doc(db, `users/${user.uid}`);
+			getDoc(userRef).then((snapshot) => {
+				if (!snapshot.exists()) {
+					setDoc(snapshot.ref, {
+						name: user?.displayName,
+						photoUrl: user?.photoURL,
+						timestamp: serverTimestamp(),
+					});
+				}
+			});
+		}
 	}, [user]);
 
 	return user;
