4cf1d8d1237b3566331a4b910344d0ffec6bbdf0
Henry Betancourth <69738617+noelys215@users.noreply.github.com>
2023-07-22 21:07:16 -0400

    continue sidebar component

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,4 +1,4 @@
-import { ExitToApp } from '@mui/icons-material';
+import { Add, ExitToApp, SearchOutlined } from '@mui/icons-material';
 import { Avatar, IconButton } from '@mui/material';
 
 export const Sidebar = ({ user }) => {
@@ -15,6 +15,19 @@ export const Sidebar = ({ user }) => {
 						<ExitToApp />
 					</IconButton>
 				</div>
+			</div>
+			{/* Search */}
+			<div className="sidebar__search">
+				<form className="sidebar__search--container">
+					<SearchOutlined />
+					<input type="text" placeholder="Search for users or rooms..." id="search" />
+				</form>
+			</div>
+			{/* Create Room Button */}
+			<div className="sidebar__chat--addRoom">
+				<IconButton>
+					<Add />
+				</IconButton>
 			</div>
 		</div>
 	);
