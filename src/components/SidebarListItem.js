7583ff78eac7c25ab3f6364902ce64f8fce395a0
Henry Betancourth <69738617+noelys215@users.noreply.github.com>
2023-07-25 20:01:55 -0400

    continue SidebarListItem.js component

--- src/components/SidebarListItem.js
+++ src/components/SidebarListItem.js
@@ -1,7 +1,23 @@
+import { Avatar } from '@mui/material';
+import Link from 'next/link';
+
 export const SidebarListItem = ({ item }) => {
 	return (
-		<div>
-			<></>
-		</div>
+		<Link className="link" href={`/?roomId=${item?.id}`}>
+			<div className="sidebar__chat">
+				<div className="avatar__container">
+					<Avatar
+						src={
+							item?.photoURL ||
+							`http://avatars.dicebear.com/api/jdenticon/${item?.id}.svg`
+						}
+						style={{ width: 45, height: 45 }}
+					/>
+				</div>
+				<div className="sidebar__chat--info">
+					<h2>{item.name}</h2>
+				</div>
+			</div>
+		</Link>
 	);
 };
