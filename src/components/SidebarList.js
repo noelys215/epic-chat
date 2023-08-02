93826684a98d8815199da92b5f8bfd167e3011e6
Henry Betancourth <69738617+noelys215@users.noreply.github.com>
2023-08-02 19:51:54 -0400

    add search filtering

--- src/components/SidebarList.js
+++ src/components/SidebarList.js
@@ -1,5 +1,20 @@
 import { CircularProgress } from '@mui/material';
 import { SidebarListItem } from './SidebarListItem';
+import { CancelOutlined, SearchOutlined } from '@mui/icons-material';
+
+const NoSearchResults = ({ title }) => {
+	return (
+		<div className="no-result">
+			<div>
+				<SearchOutlined />
+				<div className="cancel-root">
+					<CancelOutlined />
+				</div>
+			</div>
+			<h2>No {title}</h2>
+		</div>
+	);
+};
 
 export const SidebarList = ({ title, data }) => {
 	/* Spinner */
@@ -8,6 +23,8 @@ export const SidebarList = ({ title, data }) => {
 			<CircularProgress />
 		</div>
 	);
+	/* No Search Results Image */
+	if (!data?.length && title === 'Search Results') return <NoSearchResults title={title} />;
 
 	return (
 		<div className="sidebar__chat--container">
