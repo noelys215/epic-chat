56e51f5377dbaa8e49d85b4e1f478959a0d994b1
Henry Betancourth <69738617+noelys215@users.noreply.github.com>
2023-08-04 20:55:44 -0400

    create useRoom.js hook

--- src/hooks/useRoom.js
+++ src/hooks/useRoom.js
@@ -1,7 +1,19 @@
-export const useRoom = ({ roomId, userId }) => {
-	return (
-		<div>
-			<></>
-		</div>
-	);
+import { db } from '@/utils/firebase';
+import { doc } from 'firebase/firestore';
+import { useDocument } from 'react-firebase-hooks/firestore';
+
+export const useRoom = (roomId, userId) => {
+	const isUserRoom = roomId?.includes(userId);
+	const collectionId = isUserRoom ? 'users' : 'rooms';
+	const docId = isUserRoom ? roomId.replace(userId, '') : roomId;
+	const [snapshot] = useDocument(docId ? doc(db, `${collectionId}/${docId}`) : null);
+
+	!snapshot?.exists() && null;
+
+	return {
+		id: snapshot?.id,
+		photoURL:
+			snapshot?.photoURL || `http://avatars.dicebear.com/api/jdenticon/${snapshot?.id}.svg`,
+		...snapshot?.data(),
+	};
 };
