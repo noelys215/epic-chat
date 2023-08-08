ab8596d32f992c102c7425b09d9b72fbedea8cb2
Henry Betancourth <69738617+noelys215@users.noreply.github.com>
2023-08-07 20:31:01 -0400

    create MediaPreview.js component

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -2,12 +2,35 @@ import { useRoom } from '@/hooks/useRoom';
 import { AddPhotoAlternate, MoreVert } from '@mui/icons-material';
 import { Avatar, IconButton, Menu, MenuItem } from '@mui/material';
 import { useRouter } from 'next/router';
+import { useState } from 'react';
+import { MediaPreview } from './MediaPreview';
 
 export const Chat = ({ user }) => {
 	const router = useRouter();
 	const roomId = router.query.roomId ?? '';
 	const userId = user?.uid;
 	const room = useRoom(roomId, userId);
+	/* State */
+	const [image, setImage] = useState(null);
+	const [src, setSrc] = useState('');
+	/* Handlers */
+	const showPreview = (e) => {
+		const file = e.target.files[0];
+
+		if (file) {
+			setImage(file);
+			const reader = new FileReader();
+			reader.readAsDataURL(file);
+			reader.onload = () => {
+				setSrc(reader.result);
+			};
+		}
+	};
+
+	const closePreview = () => {
+		setSrc('');
+		setImage(null);
+	};
 
 	!room && null;
 	return (
@@ -22,7 +45,13 @@ export const Chat = ({ user }) => {
 					<h3>{room?.name}</h3>
 				</div>
 				<div className="chat__header--right">
-					<input id="image" style={{ display: 'none' }} accept="image/*" type="file" />
+					<input
+						id="image"
+						style={{ display: 'none' }}
+						accept="image/*"
+						type="file"
+						onChange={showPreview}
+					/>
 					<IconButton>
 						<label style={{ cursor: 'pointer', height: 24 }} htmlFor="image">
 							<AddPhotoAlternate />
@@ -38,6 +67,9 @@ export const Chat = ({ user }) => {
 					</Menu>
 				</div>
 			</div>
+
+			{/* Media Preview */}
+			<MediaPreview src={src} closePreview={closePreview} />
 		</div>
 	);
 };
