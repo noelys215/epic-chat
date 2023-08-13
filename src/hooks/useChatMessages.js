133285bb265ded665fde1989f2a8cc43c078e7e4
Henry Betancourth <69738617+noelys215@users.noreply.github.com>
2023-08-12 20:47:35 -0400

    create useChatMessages.js hook

---
+++ src/hooks/useChatMessages.js
@@ -0,0 +1,18 @@
+import { db } from '@/utils/firebase';
+import { collection, orderBy, query } from 'firebase/firestore';
+import { useCollection } from 'react-firebase-hooks/firestore';
+
+export const useChatMessages = (roomId) => {
+	const [snapshot] = useCollection(
+		roomId
+			? query(collection(db, `rooms/${roomId}/messages`), orderBy('timestamp', 'asc'))
+			: null
+	);
+
+	const messages = snapshot?.docs.map((doc) => ({
+		id: doc?.id,
+		...doc?.data(),
+	}));
+
+	return messages;
+};
